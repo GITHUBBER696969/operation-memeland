cf1c6497df9155b8658462e52374d62a1ea954a5
tabualkher <t_abualkher@hotmail.com>
2022-06-29 15:13:20 +0300

    commit suhal

---
+++ main/main.js
@@ -0,0 +1,18 @@
+function funny (){
+    let funny= document.querySelector("funny");
+    if (funny==funny ){
+        alert("it used to be  but his ass got smaller as ours got bigger ");
+    }
+    }
+    function funny2 (){
+    let funny2 = document.querySelector("funny2");
+    if (funny2 == funny2){
+        alert("the smell of his food was sometimes very nice ");
+    }
+    }
+    function funny3(){
+    let funny3= document.querySelector("funny3");
+    if (funny3 == funny3 ){
+        alert(" it was a blast and it is still holding strong to this day   ");
+    }
+    }
